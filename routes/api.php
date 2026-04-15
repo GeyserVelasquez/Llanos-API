b8ae69184343b416c490d282091dc1a938dfe3cb
GeyserVelasquez <gvelasquez.9312@unimar.edu.ve>
2026-04-15 14:12:25 -0400

    Init Laravel Framework

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+
+    Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    require __DIR__.'/auth.php';
+
+});
