d87e099237ec7abc297e6302e3afccf96c75a5a7
GeyserVelasquez <gvelasquez.9312@unimar.edu.ve>
2026-04-20 14:49:25 -0400

    LookUpFactories Added

---
+++ database/factories/AbortTypeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AbortType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<AbortType>
+ */
+class AbortTypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->word;
+        $code = mb_strtoupper(mb_substr($name, 0, 2));
+
+        return [
+            'code' => $code,
+            'name' => $name,
+        ];
+    }
+}
---
+++ database/factories/OutcomeTypeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\OutcomeType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<OutcomeType>
+ */
+class OutcomeTypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->word();
+        $code = mb_strtoupper(mb_substr($name, 0, 2));
+
+        return [
+            'code' => $code,
+            'name' => $name,
+        ];
+    }
+}
